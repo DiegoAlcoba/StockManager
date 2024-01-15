3433f2dde55203af2266c27aef5fee81ca3f4413
DiegoAlcoba <diegoalcobaarias@gmail.com>
2024-01-15 13:30:02 +0100

    pedidoBD avanzado y detallePedidoBD creado

--- src/main/java/modelo/db/OperacionesBD_pedido.java
+++ src/main/java/modelo/db/OperacionesBD_pedido.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import modelo.entidad.Pedido;
+
 /**
  *
  * @author diego
@@ -12,6 +14,26 @@ import java.sql.SQLException;
 public class OperacionesBD_pedido {
     
     /* Añadir un pedido realizado a la base de datos */
+    public static void addPedido_BD(Pedido pedido) {
+        String query = "INSERT INTO pedido (userId, fecha, precioTotal, distribId) VALUES (?, ?, ?, ?)";
+                
+        try (Connection conn = Conexion.getConexion();
+         PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            
+            // Se establece el valor a cada uno de los parámetro de la sentencia
+            preparedStatement.setInt(1, pedido.getUsuarioId()); // Falta implementación en constructor
+            preparedStatement.setDate(2, pedido.getFecha());
+            preparedStatement.setBigDecimal(3, pedido.getPrecioTotal());
+            preparedStatement.setInt(4, pedido.getDistribuidor());
+            
+            // Ejecuta la consulta SQL
+            preparedStatement.executeUpdate();
+            System.out.println("Pedido añadido correctamente.");
+        
+        } catch (SQLException e) {
+            System.err.println("Error al insertar el pedido en la base de datos: " + e.getMessage());
+        }
+    }
 
     /* Recuperar un pedido realizado de la base de datos */
 
