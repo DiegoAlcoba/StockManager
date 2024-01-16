9f74d46399f2808aaeadf2620b2d4f808d344133
DiegoAlcoba <diegoalcobaarias@gmail.com>
2024-01-16 16:42:24 +0100

    getConexion y closeConexion comentados - no son necesarios

--- src/main/java/vista/Ventanal.java
+++ src/main/java/vista/Ventanal.java
@@ -1289,9 +1289,10 @@ public class Ventanal extends javax.swing.JFrame {
             }
         });
         
-        Connection con = Conexion.getConexion();
+        /* No es necesario */
+        //Connection con = Conexion.getConexion();
         //Desconexión de la BD
-        Conexion.closeConexion(con);
+        //Conexion.closeConexion(con);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
