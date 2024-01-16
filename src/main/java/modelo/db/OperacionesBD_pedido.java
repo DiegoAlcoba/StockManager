177422aaa55558bd7e43d7c314e6ebe60cf14ac2
DiegoAlcoba <diegoalcobaarias@gmail.com>
2024-01-16 11:42:20 +0100

    Obtener todos los pedidos realizados hecho

--- src/main/java/modelo/db/OperacionesBD_pedido.java
+++ src/main/java/modelo/db/OperacionesBD_pedido.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import modelo.entidad.Distribuidor;
 import modelo.entidad.Pedido;
 import modelo.entidad.Producto;
 
@@ -74,5 +75,59 @@ public class OperacionesBD_pedido {
         }
     }
 
+    /* Devuelve un vector con los pedidos realizados */
+    public static Pedido[] getListaPedidos_BD() {
+        int totalPeds = nPedidos();
 
+        Pedido[] pedidos = new Pedido[totalPeds];
+
+        String query = "SELECT * FROM pedido";
+                
+        try (Connection conn = Conexion.getConexion();
+         PreparedStatement preparedStatement = conn.prepareStatement(query);
+         ResultSet resultSet = preparedStatement.executeQuery()) {
+            
+            int i = 0;
+
+            while (resultSet.next()) {
+                Pedido pedido = new Pedido();
+                
+                pedido.setPedidoId(resultSet.getInt("pedidoId"));
+                pedido.setUserId(resultSet.getInt("userId"));
+                pedido.setFecha(resultSet.getDate("fecha"));
+                pedido.setPrecioTotal(resultSet.getBigDecimal("precioTotal"));
+                pedido.setDistribuidor(resultSet.getString("distribId"));
+
+                pedidos[i++] = pedido;
+            }
+
+            return pedidos;
+        
+        } catch (SQLException e) {
+            System.err.println("Error al obtener los distribuidores: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    //Método que devuelve el número de pedidos totales en la BD
+    private static int nPedidos() {
+        int cantidad = 0;
+
+        String query = "SELECT COUNT(*) AS cantidad FROM pedido";
+
+        try (Connection conn = Conexion.getConexion();
+         PreparedStatement preparedStatement = conn.prepareStatement(query);
+         ResultSet countResultSet = preparedStatement.executeQuery()) {
+            
+            if (countResultSet.next()) {
+                cantidad = countResultSet.getInt("cantidad");
+            }
+        
+        } catch (SQLException e) {
+            System.err.println("Error al obtener la cantidad de pedidos realizados: " + e.getMessage());
+        }
+
+        return cantidad;
+    }
 }
